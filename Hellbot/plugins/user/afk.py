c8adac137d9856deee36adf7ae0201e3ae628168
SiamRin33 <149977276+SiamRin33@users.noreply.github.com>
2024-01-04 22:55:13 +0600

    Update afk.py

--- Hellbot/plugins/user/afk.py
+++ Hellbot/plugins/user/afk.py
@@ -3,16 +3,15 @@ import random
 import time
 
 from pyrogram import Client, filters
-from pyrogram.enums import MessageMediaType
+from pyrogram.enums import MessageMediaType, ChatType
 from pyrogram.types import Message
 
 from Hellbot.core import Config, db, hellbot
 from Hellbot.functions.formatter import add_to_dict, get_from_dict, readable_time
 
-from . import HelpMenu, custom_handler, on_message
+from . import HelpMenu, custom_handler, on_message, group_only
 
 afk_quotes = [
-    "🚶‍♂️ Taking a break, be back soon!",
     "⏳ AFK - Away From the Keyboard momentarily.",
     "🔜 Stepped away, but I'll return shortly.",
     "👋 Gone for a moment, not forgotten.",
@@ -22,22 +21,16 @@ afk_quotes = [
     "🌈 Away from the screen, catching a breath.",
     "💤 Offline for a moment, but still here in spirit.",
     "🚀 Exploring the real world, back in a moment!",
-    "🍵 Taking a tea break, back shortly!",
-    "🌙 Resting my keyboard, back after a short nap.",
     "🚶‍♀️ Stepping away for a moment of peace.",
-    "🎵 AFK but humming along, back shortly!",
-    "🌞 Taking a sunshine break, back soon!",
-    "🌊 Away, catching some waves of relaxation.",
     "🚪 Temporarily closed, be back in a bit!",
     "🌸 Taking a moment to smell the digital roses.",
-    "🍃 Stepped into the real world for a while.",
 ]
 
 
 @on_message("afk")
 async def afk(_, message: Message):
     if await db.is_afk(message.from_user.id):
-        return await hellbot.delete(message, "🙄 𝖨'𝗆 𝖺𝗅𝗋𝖾𝖺𝖽𝗒 𝖠𝖥𝖪!")
+        return await hellbot.delete(message, "I'm already AFK!")
 
     media_type = None
     media = None
@@ -65,7 +58,7 @@ async def afk(_, message: Message):
     reason = await hellbot.input(message)
     reason = reason if reason else "Not specified"
 
-    await hellbot.delete(message, "🫡 𝖦𝗈𝗂𝗇𝗀 𝖠𝖥𝖪! 𝖲𝖾𝖾 𝗒𝖺'𝗅𝗅 𝗅𝖺𝗍𝖾𝗋.")
+    await hellbot.delete(message, "🫡 Going AFK! See ya'll later.")
     await db.set_afk(message.from_user.id, reason, media, media_type)
     await hellbot.check_and_log(
         "afk",
@@ -75,16 +68,25 @@ async def afk(_, message: Message):
     add_to_dict(Config.AFK_CACHE, [message.from_user.id, message.chat.id])
 
 
-@custom_handler(filters.incoming & filters.mentioned & ~filters.bot & ~filters.me & ~filters.service)
+@custom_handler(
+    filters.incoming
+    & ~filters.bot
+    & ~filters.service
+)
 async def afk_watch(client: Client, message: Message):
     afk_data = await db.get_afk(client.me.id)
     if not afk_data:
         return
+
     if message.from_user.id == afk_data["user_id"]:
         return
 
+    if message.chat.type in group_only:
+        if not message.mentioned:
+            return
+
     afk_time = readable_time(round(time.time() - afk_data["time"]))
-    caption = f"**{random.choice(afk_quotes)}**\n\n**💫 𝖱𝖾𝖺𝗌𝗈𝗇:** {afk_data['reason']}\n**⏰ 𝖠𝖥𝖪 𝖥𝗋𝗈𝗆:** `{afk_time}`"
+    caption = f"**{random.choice(afk_quotes)}**\n\n**💫 Reason:** {afk_data['reason']}\n**⏰ AFK From:** `{afk_time}`"
 
     if afk_data["media_type"] == "animation":
         sent = await client.send_animation(message.chat.id, afk_data["media"], caption, True)
@@ -103,9 +105,11 @@ async def afk_watch(client: Client, message: Message):
     else:
         sent = await message.reply_text(caption)
 
+    link = message.link if message.chat.type in group_only else "No DM Link"
+
     await hellbot.check_and_log(
         "afk",
-        f"{message.from_user.mention} mentioned you when you were AFK! \n\n**Link:** {message.link}",
+        f"{message.from_user.mention} mentioned you when you were AFK! \n\n**Link:** {link}",
     )
     try:
         data = get_from_dict(Config.AFK_CACHE, [afk_data["user_id"], message.chat.id])
@@ -126,7 +130,7 @@ async def remove_afk(_, message: Message):
         total_afk_time = readable_time(round(time.time() - data["time"]))
 
         hell = await message.reply_text(
-            f"🫡 **𝖡𝖺𝖼𝗄 𝗍𝗈 𝗏𝗂𝗋𝗍𝗎𝖺𝗅 𝗐𝗈𝗋𝗅𝖽! \n\n⌚ Was away for:** `{total_afk_time}`"
+            f"🫡 **Back to virtual world! \n\n⌚ Was away for:** `{total_afk_time}`"
         )
         await message.delete()
 
