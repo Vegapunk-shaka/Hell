7fee2bc22a5b87133d179dbfcb136d639198a7b2
Siam <ssiam4235@gmail.com>
2024-03-11 00:46:20 +0600

    Update admins.py

--- Hellbot/plugins/user/admins.py
+++ Hellbot/plugins/user/admins.py
@@ -283,31 +283,31 @@ async def unmute(client: Client, message: Message):
     allow_stan=False,
 )
 async def startmute(client: Client, message: Message):
-    xx = await hellbot.eor("`Muting...`")
+    xx = await hellbot.delete(message,f"**Muting...**", 30)
     if input_ := hellbot.pattern_match.group(1).strip():
         try:
             userid = await hellbot.client.parse_id(input_)
         except Exception as x:
             return await xx.edit(str(x))
-    elif event.reply_to_msg_id:
-        reply = await event.get_reply_message()
+    elif hellbot.reply_to_msg_id:
+        reply = await hellbot.get_reply_message()
         userid = reply.sender_id
         if reply.out or userid in [hellbot.me.id, asst.me.id]:
-            return await xx.eor("`You cannot mute yourself or your assistant bot.`")
-    elif event.is_private:
-        userid = event.chat_id
+            return await hellbot.delete(message,f"**You cannot mute yourself or your assistant bot.**", 30)
+    elif hellbot.is_private:
+        userid = hellbot.chat_id
     else:
-        return await xx.eor("`Reply to a user or add their userid.`", time=5)
-    chat = await event.get_chat()
+        return await hellbot.(message,f"**Reply to a user or add their userid.**", time=5)
+    chat = await hellbot.get_chat()
     if "admin_rights" in vars(chat) and vars(chat)["admin_rights"] is not None:
         if not chat.admin_rights.delete_messages:
-            return await xx.eor("`No proper admin rights...`", time=5)
-    elif "creator" not in vars(chat) and not event.is_private:
-        return await xx.eor("`No proper admin rights...`", time=5)
-    if is_muted(event.chat_id, userid):
-        return await xx.eor("`This user is already muted in this chat.`", time=5)
-    mute(event.chat_id, userid)
-    await xx.eor("`Successfully muted...`", time=3)
+            return await hellbot.(message,f"**No proper admin rights...**", time=5)
+    elif "creator" not in vars(chat) and not hellbot.is_private:
+        return await hellbot.(message,f"**No proper admin rights...**", time=5)
+    if is_muted(hellbot.chat_id, userid):
+        return await hellbot.(message,f"**This user is already muted in this chat.**", time=5)
+    mute(hellbot.chat_id, userid)
+    await hellbot.(message,f"**Successfully muted...**", time=3)
 
 
 @on_message(
