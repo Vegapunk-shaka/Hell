a1946cdf8765188d0a971aef6b545928d20fbacb
Siam <ssiam4235@gmail.com>
2024-03-17 23:07:55 +0600

    Update bot.py

--- Hellbot/plugins/bot/bot.py
+++ Hellbot/plugins/bot/bot.py
@@ -34,7 +34,7 @@ async def help_pm(_, message: Message):
 
 @hellbot.bot.on_message(filters.command("restart") & Config.AUTH_USERS)
 async def restart_clients(_, message: Message):
-    await message.reply_text("Restarted Bot Successfully ✅")
+    await message.reply_text("Bot Restarting....")
     try:
         if HEROKU_APP:
             try:
