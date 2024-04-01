3bdd6ada8f49b81dbda8a819fac971ed71117119
Siam <ssiam4235@gmail.com>
2024-04-02 03:49:13 +0600

    Update afk.py

--- Hellbot/plugins/user/afk.py
+++ Hellbot/plugins/user/afk.py
@@ -3,24 +3,34 @@ import random
 import time
 
 from pyrogram import Client, filters
-from pyrogram.enums import MessageMediaType, ChatType
+from pyrogram.enums import MessageMediaType
 from pyrogram.types import Message
 
 from Hellbot.core import Config, db, hellbot
 from Hellbot.functions.formatter import add_to_dict, get_from_dict, readable_time
 
-from . import HelpMenu, custom_handler, on_message, group_only
+from . import HelpMenu, custom_handler, group_only, on_message
 
 afk_quotes = [
     "🚶‍♂️ Taking a break, be back soon!",
-    "⏳ Taking a break, be back soon!",
-    "🔜 Taking a break, be back soon!",
-    "👋 Taking a break, be back soon!",
-    "🌿 Taking a break, be back soon!",
-    "📵 Taking a break, be back soon!",
-    "⏰ Taking a break, be back soon!",
-    "🌈 Taking a break, be back soon!",
-    "💤 Taking a break, be back soon!",
+    "⏳ AFK - Away From the Keyboard momentarily.",
+    "🔜 Stepped away, but I'll return shortly.",
+    "👋 Gone for a moment, not forgotten.",
+    "🌿 Taking a breather, back in a bit.",
+    "📵 Away for a while, feel free to leave a message!",
+    "⏰ On a short break, back shortly.",
+    "🌈 Away from the screen, catching a breath.",
+    "💤 Offline for a moment, but still here in spirit.",
+    "🚀 Exploring the real world, back in a moment!",
+    "🍵 Taking a tea break, back shortly!",
+    "🌙 Resting my keyboard, back after a short nap.",
+    "🚶‍♀️ Stepping away for a moment of peace.",
+    "🎵 AFK but humming along, back shortly!",
+    "🌞 Taking a sunshine break, back soon!",
+    "🌊 Away, catching some waves of relaxation.",
+    "🚪 Temporarily closed, be back in a bit!",
+    "🌸 Taking a moment to smell the digital roses.",
+    "🍃 Stepped into the real world for a while.",
 ]
 
 
@@ -35,41 +45,32 @@ async def afk(_, message: Message):
     if message.reply_to_message and message.reply_to_message.media:
         if message.reply_to_message.media == MessageMediaType.ANIMATION:
             media_type = "animation"
-            media = message.reply_to_message.animation.file_id
         elif message.reply_to_message.media == MessageMediaType.AUDIO:
             media_type = "audio"
-            media = message.reply_to_message.audio.file_id
         elif message.reply_to_message.media == MessageMediaType.PHOTO:
             media_type = "photo"
-            media = message.reply_to_message.photo.file_id
         elif message.reply_to_message.media == MessageMediaType.STICKER:
             media_type = "sticker"
-            media = message.reply_to_message.sticker.file_id
         elif message.reply_to_message.media == MessageMediaType.VIDEO:
             media_type = "video"
-            media = message.reply_to_message.video.file_id
         elif message.reply_to_message.media == MessageMediaType.VOICE:
             media_type = "voice"
-            media = message.reply_to_message.voice.file_id
+
+        media = await message.reply_to_message.forward(Config.LOGGER_ID)
 
     reason = await hellbot.input(message)
     reason = reason if reason else "Not specified"
 
     await hellbot.delete(message, "🫡 Going AFK! See ya'll later.")
-    await db.set_afk(message.from_user.id, reason, media, media_type)
+    await db.set_afk(message.from_user.id, reason, media.id, media_type)
     await hellbot.check_and_log(
         "afk",
         f"Going AFK! \n\n**Reason:** `{reason}`",
-        media,
     )
     add_to_dict(Config.AFK_CACHE, [message.from_user.id, message.chat.id])
 
 
-@custom_handler(
-    filters.incoming
-    & ~filters.bot
-    & ~filters.service
-)
+@custom_handler(filters.incoming & ~filters.bot & ~filters.service)
 async def afk_watch(client: Client, message: Message):
     afk_data = await db.get_afk(client.me.id)
     if not afk_data:
@@ -86,19 +87,26 @@ async def afk_watch(client: Client, message: Message):
     caption = f"**{random.choice(afk_quotes)}**\n\n**💫 Reason:** {afk_data['reason']}\n**⏰ AFK From:** `{afk_time}`"
 
     if afk_data["media_type"] == "animation":
-        sent = await client.send_animation(message.chat.id, afk_data["media"], caption, True)
-    elif afk_data["media_type"] == "audio":
-        sent = await message.reply_audio(afk_data["media"], caption=caption)
-    elif afk_data["media_type"] == "photo":
-        sent = await message.reply_photo(afk_data["media"], caption=caption)
+        media = await client.get_messages(Config.LOGGER_ID, afk_data["media"])
+        sent = await client.send_animation(
+            message.chat.id, media.animation.file_id, caption, True
+        )
+
+    elif afk_data["media_type"] in ["audio", "photo", "video", "voice"]:
+        sent = await client.copy_message(
+            message.chat.id,
+            Config.LOGGER_ID,
+            afk_data["media"],
+            caption,
+            reply_to_message_id=message.id,
+        )
+
     elif afk_data["media_type"] == "sticker":
-        await client.download_media(afk_data["media"], "afk.png")
+        media = await client.get_messages(Config.LOGGER_ID, afk_data["media"])
+        await client.download_media(media, "afk.png")
         sent = await message.reply_photo("afk.png", caption=caption)
         os.remove("afk.png")
-    elif afk_data["media_type"] == "video":
-        sent = await message.reply_video(afk_data["media"], caption=caption)
-    elif afk_data["media_type"] == "voice":
-        sent = await message.reply_voice(afk_data["media"], caption=caption)
+
     else:
         sent = await message.reply_text(caption)
 
@@ -136,10 +144,6 @@ async def remove_afk(_, message: Message):
             "afk",
             f"Returned from AFK! \n\n**Time:** `{total_afk_time}`\n**Link:** {hell.link}",
         )
-        try:
-            os.remove(data["media"])
-        except:
-            pass
 
 
 HelpMenu("afk").add(
