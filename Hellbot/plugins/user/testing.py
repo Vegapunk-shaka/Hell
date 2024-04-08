11dcbc4f5c193aa267c766c06624bebd63274cf7
Siam <ssiam4235@gmail.com>
2024-04-09 05:59:50 +0600

    Update testing.py

--- Hellbot/plugins/user/testing.py
+++ Hellbot/plugins/user/testing.py
@@ -2,21 +2,22 @@ from pyrogram import Client, filters
 from PIL import Image
 import requests
 from io import BytesIO
-from . import HelpMenu, hellbot, on_message
+import os
 
+# Initialize the Pyrogram Client
+app = Client("my_image_upscale_bot")
 
-@on_message("ups", allow_stan=True)
-async def upscale_image(client, message):
+# Function to upscale the image
+def upscale_image(client, message):
     chat_id = message.chat.id
-
+    
     if message.photo is None:
-        # Handle the case where message.photo is None
-        # You can send a message to the user or take appropriate action
+        client.send_message(chat_id, "Please send an image to upscale.")
         return
-
+    
     file_id = message.photo.file_id
 
-    file = await client.get_photo(file_id)
+    file = client.get_photo(file_id)
     image_url = file.file_path
 
     response = requests.get(image_url)
@@ -28,4 +29,12 @@ async def upscale_image(client, message):
     image.save(output, format='PNG')
     output.seek(0)
 
-    await client.send_photo(chat_id, photo=output)
+    client.send_photo(chat_id, photo=output)
+
+# Registering the upscale_image function as a message handler
+@app.on_message("ups", allow_stan=True)
+async def upscale_command(client, message):
+    await upscale_image(client, message)
+
+# Start the bot
+app.run()
