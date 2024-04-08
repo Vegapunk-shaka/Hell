4e8a79b9b72399a7f5a3620f45f2ac4d23e565dc
Siam <ssiam4235@gmail.com>
2024-04-09 05:51:54 +0600

    Update testing.py

--- Hellbot/plugins/user/testing.py
+++ Hellbot/plugins/user/testing.py
@@ -6,17 +6,17 @@ from . import HelpMenu, hellbot, on_message
 
 
 @on_message("ups", allow_stan=True)
-def upscale_image(client, message):
+async def upscale_image(client, message):
     chat_id = message.chat.id
-    
+
     if message.photo is None:
         # Handle the case where message.photo is None
         # You can send a message to the user or take appropriate action
         return
-    
+
     file_id = message.photo.file_id
 
-    file = client.get_photo(file_id)
+    file = await client.get_photo(file_id)
     image_url = file.file_path
 
     response = requests.get(image_url)
@@ -28,4 +28,4 @@ def upscale_image(client, message):
     image.save(output, format='PNG')
     output.seek(0)
 
-    client.send_photo(chat_id, photo=output)
+    await client.send_photo(chat_id, photo=output)
